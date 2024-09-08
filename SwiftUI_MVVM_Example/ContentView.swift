c2a9b4e63960c54c95df352bbd83de97dab48a9e
kalashnikovaYla <kalashnikova-yla@mail.ru>
2024-09-08 17:12:40 +0500

    initial

---
+++ SwiftUI_MVVM_Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_MVVM_Example
+//
+//  Created by Юлия  on 08.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
---
+++ SwiftUI_MVVM_Example/SwiftUI_MVVM_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_ExampleApp.swift
+//  SwiftUI_MVVM_Example
+//
+//  Created by Юлия  on 08.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
